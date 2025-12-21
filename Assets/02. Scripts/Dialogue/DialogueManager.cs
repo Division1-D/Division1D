aa2df3b2e99df2ca3c42ae0c31f580a79ab66493
편오 <108973850+Editor0518@users.noreply.github.com>
2025-12-21 23:34:05 +0900

    선택지 UI&애니메이션 추가

--- Assets/02. Scripts/Dialogue/DialogueManager.cs
+++ Assets/02. Scripts/Dialogue/DialogueManager.cs
@@ -18,6 +18,11 @@ public class DialogueManager : MonoBehaviour
     private int currentDialogueIndex=0; // 위 dialogues에서 현재 출력할 인덱스
     Dialogue currentDialogue; //현재 타이핑 해야하는 다이얼로그 정보
     bool isDialogueTyping = false; //현재 타이핑 중인지(타이핑 중에 클릭 시, 모든 텍스트 띄워주기 위함)
+    bool isChoiceMode = false;
+
+    public GameObject choiceWhole;
+    public List<TMP_Text> choiceTextList;
+    private List<int> choiceIndexList = null;
     
     [System.Serializable]
     public struct Dialogue
@@ -30,19 +35,23 @@ public class DialogueManager : MonoBehaviour
     void Start()
     {
         canvas=GetComponent<Canvas>();
+        CloseChoiceMode();
         StartDialogue(dialogues); //test only
     }
-
     
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            NextDialogue();
+            if (isChoiceMode)
+            {
+                return;
+            }
+            else  NextDialogue();
         }
     }
 
-    public void StartDialogue(List<Dialogue> newDialogues)
+    void StartDialogue(List<Dialogue> newDialogues)
     {
         canvas.enabled = true;
         this.dialogues = newDialogues;
@@ -50,8 +59,8 @@ public class DialogueManager : MonoBehaviour
         NextDialogue();
     }
 
-    public void NextDialogue()
-    {
+    void NextDialogue()
+    {//여기 복잡한 if문은 나중에 가능하면 리팩터링하기...
         if (isDialogueTyping)
         {
             StopCoroutine("TypeDialogue");
@@ -62,18 +71,41 @@ public class DialogueManager : MonoBehaviour
             isDialogueTyping = false; //타이핑 다 된 상태
             currentDialogueIndex++; //현재 인덱스 ++
             
+            if (dialogues[currentDialogueIndex].name.Equals("선택지"))
+            {
+                StartChoiceMode();
+            }
             return; //리턴 (한번 더 클릭해야 다음꺼 타이핑)
         }
-
+        
         if (currentDialogueIndex >= dialogues.Count)
         {
-            currentDialogueIndex = 0;
-            canvas.enabled = false;
+            EndDialogue();
             return;
         }
-        StartCoroutine("TypeDialogue", (dialogues[currentDialogueIndex]));
+
+        
+        if (dialogues[currentDialogueIndex].name.Equals("선택지"))
+        {
+            StartChoiceMode();
+            //현재 다이얼로그 인덱스 등을 절대 초기화하지 않는다.
+            return;
+        }
+        if (dialogues[currentDialogueIndex].name.Equals("종료"))
+        {
+            EndDialogue();
+            return;
+        }
+        
+        StartCoroutine("TypeDialogue", dialogues[currentDialogueIndex]);
     }
 
+    void EndDialogue()
+    {
+        currentDialogueIndex = 0;
+        canvas.enabled = false;
+    }
+    
     IEnumerator TypeDialogue(Dialogue dialogue)
     {
         isDialogueTyping = true;
@@ -91,6 +123,91 @@ public class DialogueManager : MonoBehaviour
         
         isDialogueTyping = false;
         currentDialogueIndex++;
+        if (dialogues[currentDialogueIndex].name.Equals("선택지"))
+        {
+            NextDialogue();
+        }
+    }
+    
+    /*
+     [ 사실 걱정되는 거지? ]:0
+     [ 책임…? ]:0
+     */
+
+    void StartChoiceMode()
+    {
+        choiceWhole.SetActive(true);
+        string[] choices=dialogues[currentDialogueIndex].text.Split('\n');
+        Debug.Log(choices.Length+"개의 선택지 발견됨!");
+        choiceIndexList = new();
+
+        for (int i = 0; i < choices.Length; i++)
+        {
+            //예시    [ 사실 걱정되는 거지? ]:0
+            string[] choiceSplit = choices[i].Split(" ]:");
+            string choiceName = choiceSplit[0].Substring(1,choiceSplit[0].Length-1);
+            int index = int.Parse(choiceSplit[1]);
+            choiceIndexList.Add(index); //예시: 0
+            choiceTextList[i].text = choiceName; //예시: 
+        }
+
+        StartCoroutine(ChoiceOn(choices.Length));
+    }
+
+    IEnumerator ChoiceOn(int choiceLength)
+    {
+        for (int i = 0; i < choiceIndexList.Count; i++)
+        {
+            if(i<choiceLength){
+                choiceTextList[i].transform.parent.gameObject.SetActive(true);
+                yield return new WaitForSeconds(0.25f);
+            }
+            else
+            {
+                choiceTextList[i].transform.parent.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    IEnumerator ChoiceOff(int choiceSelected)
+    {
+        
+        for (int i = 0; i < choiceIndexList.Count; i++)
+        {
+            if(i==choiceSelected){
+                choiceTextList[i].color=Color.yellow;
+            }
+            else
+            {
+                choiceTextList[i].transform.parent.GetComponent<Animator>().SetTrigger("off");
+            }
+        }
+        yield return new WaitForSeconds(0.5f);
+        choiceTextList[choiceSelected].transform.parent.GetComponent<Animator>().SetTrigger("off");
+        yield return new WaitForSeconds(1f);
+        CloseChoiceMode();
+    }
+   
+
+    public void ChoiceSelect(int index)
+    {
+        currentDialogueIndex = choiceIndexList[index];
+        StartCoroutine(ChoiceOff(index));
+        //CloseChoiceMode();
+        NextDialogue();
+    }
+
+    void CloseChoiceMode()
+    {
+        isChoiceMode = false;
+        
+        for (int i = 0; i < choiceTextList.Count; i++)
+        {
+            choiceTextList[i].color=Color.white;
+            choiceTextList[i].transform.parent.gameObject.SetActive(false);
+        }
+        choiceWhole.SetActive(false);
+        
     }
     
 }
